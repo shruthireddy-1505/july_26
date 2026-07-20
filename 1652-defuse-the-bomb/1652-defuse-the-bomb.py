e4919b0fdc92af23d453462a81991044c37bf9b4
shruthireddy-1505 <shruthireddyneelannolla@gmail.com>
2026-07-20 13:53:23 +0530

    Time: 4 ms (38.29%), Space: 19.3 MB (34.25%) - LeetHub

--- 1652-defuse-the-bomb/1652-defuse-the-bomb.py
+++ 1652-defuse-the-bomb/1652-defuse-the-bomb.py
@@ -3,25 +3,24 @@ class Solution:
         if k==0:
             return [0]*len(code)
         res = [0]*len(code)
-        arr = code*2
         n = len(code)
         if k>0:
             window = sum(code[1:k+1])
             res[0] = window
             for i in range(1,len(code)):
-                window += arr[i+k] 
-                window -= arr[i]
+                window += code[(i+k)%n]
+                window -= code[i%n]
                 res[i] = window
         if k<0:
             #0 1 2 3 4 5 6 7
             #2 4 9 3 2 4 9 3
-            #k = -k
-            window = sum(arr[n+k:n])
+            k = -k
+            window = sum(code[n-k:n])
             
             for i in range(n):
                 res[i] = window
-                window += arr[n+i]
-                window -= arr[n+k+i]
+                window += code[i]
+                window -= code[(i+n-k)%n]
         return res
 
         
