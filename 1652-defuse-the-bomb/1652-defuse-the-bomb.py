d37aa0d831d599da4119738d6ce94e3de6ab4b61
shruthireddy-1505 <shruthireddyneelannolla@gmail.com>
2026-07-20 11:57:49 +0530

    Time: 0 ms (100%), Space: 19.2 MB (73.02%) - LeetHub

---
+++ 1652-defuse-the-bomb/1652-defuse-the-bomb.py
@@ -0,0 +1,32 @@
+class Solution:
+    def decrypt(self, code: List[int], k: int) -> List[int]:
+        if k==0:
+            return [0]*len(code)
+        res = [0]*len(code)
+        arr = code*2
+        n = len(code)
+        if k>0:
+            window = sum(code[1:k+1])
+            res[0] = window
+            for i in range(1,len(code)):
+                window += code[(i+k)%n]
+                window -= code[i%n]
+                res[i] = window
+        if k<0:
+            #0 1 2 3 4 5 6 7
+            #2 4 9 3 2 4 9 3
+            #k = -k
+            window = sum(arr[n+k:n])
+            
+            for i in range(n):
+                res[i] = window
+                window += arr[n+i]
+                window -= arr[n+k+i]
+        return res
+
+        
+            
+
+
+
+        
