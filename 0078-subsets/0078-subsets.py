a1d115adc67f1ec635f529dc183855fec0b15211
shruthireddy-1505 <shruthireddyneelannolla@gmail.com>
2026-07-23 10:46:13 +0530

    Time: 0 ms (100%), Space: 19.4 MB (66.86%) - LeetHub

---
+++ 0078-subsets/0078-subsets.py
@@ -0,0 +1,11 @@
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        res = []
+        n = len(nums)
+        for i in range(2**n):
+            sub = []
+            for j in range(n):
+                if (i>>j)&1:
+                    sub.append(nums[j])
+            res.append(sub)
+        return res        
