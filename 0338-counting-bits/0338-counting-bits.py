28eab7c0ab6da1a2b0f621b4cdc7a9b92218e85c
shruthireddy-1505 <shruthireddyneelannolla@gmail.com>
2026-07-27 11:23:51 +0530

    Time: 64 ms (9.05%), Space: 20.1 MB (72.48%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.py
+++ 0338-counting-bits/0338-counting-bits.py
@@ -1,5 +1,6 @@
 class Solution:
     def countBits(self, n: int) -> List[int]:
+        """
         res = [0]*(n+1)
         for i in range(n+1):
             count = 0
@@ -11,5 +12,19 @@ class Solution:
                 i = i //2
             res[dup] = count
         return res
+        """
+        res = [0]*(n+1)
+        for i in range(n+1):
+            n = i
+            count = 0
+            while n!=0:
+                if n&1!=0:
+                    count +=1
+                n>>=1
+            res[i] = count
+        return res
+            
+
+
 
         
